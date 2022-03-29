18a201ad2a8021d7de9d882b0d8797c940cc5074
Florian Lebecque <=>
2022-03-29 14:28:00 +0200

    Added read,write and delete route for agency

--- routers/rt-Agency.js
+++ routers/rt-Agency.js
@@ -4,7 +4,60 @@ const ctrl = require("../controler/Controler");
 const AgencyRouter = Express.Router();
 
 
+AgencyRouter.get("/",async (req,res)=>{
 
+    let uuid = req.query.uuid;
+
+    try {
+        let agency = await ctrl.GetAgency(uuid);
+        
+        res.json(agency);
+
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+
+
+
+
+});
+
+AgencyRouter.delete("/",async (req,res)=>{
+
+    let uuid = req.query.uuid;
+
+    try {
+        let response = await ctrl.DeleteAgency(uuid);
+        
+        res.json(response);
+
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+
+
+
+
+});
+
+AgencyRouter.post("/",async (req,res)=>{
+
+    let agency_form = req.body;
+
+    try {
+        let response = await ctrl.CreateAgency(agency_form);
+
+        if(response){
+            res.json(response);
+        }
+        
+    } catch (error) {
+        res.status(error.code).json(error.err);
+    }
+
+
+
+});
 
 let exp = {
     path:"agency",
