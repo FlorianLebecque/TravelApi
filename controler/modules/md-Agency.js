3a24d529124e77bd538306d17367b3888d4f5e88
Florian Lebecque <=>
2022-03-29 14:30:45 +0200

    added read,write and delete logic for agency

--- controler/modules/md-Agency.js
+++ controler/modules/md-Agency.js
@@ -1,9 +1,69 @@
 const Agency = require("../../models/Agency");
+const Str = require("@supercharge/strings");
 
 const AgencyFunctions = {
     test(){
         console.log("test");
+    },
+
+    async GetAgency(uuid){
+
+        let data = await Agency.findOne({where:{id:uuid}}).then(data=>{
+            return data;
+        }).catch(err=>{
+            throw {code:500,err:"Internal server error"};
+        })
+
+        
+        if(data){
+            return data;
+        }
+        throw {code:404,err:"Not found"};
+
+
+    },
+
+    async CreateAgency(agency_form){
+
+        if(!this.CheckObj(agency_form,["name","address","email","telephone"])){
+            throw {code:400,err:"Incomplete forms"};
+        }
+
+        let new_agency = Agency.build({
+            id          :Str.uuid(),
+            name        :this.Sanatize(agency_form.name),
+            address     :this.Sanatize(agency_form.address),
+            email       :this.Sanatize(agency_form.email),
+            telephone   :this.Sanatize(agency_form.telephone),
+        });
+
+        let data = await new_agency.save().then(data => {
+            return data;
+        }).catch(err=>{
+            throw {code:500,err:"Internal server error"};
+        });
+
+        return data;
+
+    },
+
+    async DeleteAgency(uuid){
+
+        let result = await Agency.destroy({where:{id:uuid}}).then(data => {
+            return data;
+        }).catch(err=>{
+            throw {code:500,err:"Internal server error"};
+        });
+
+        if(result != 0){
+            return result;
+        }
+
+        throw {code:404,err:"Not found"};
+
     }
+
+
 }
 
 
